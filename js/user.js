ea7ea608354add34bf2dbd81b57b0418df6b7292
강진수 <wide4514@ncubeworld.com>
2024-09-12 15:40:04 +0900

    get-user-info success

--- js/user.js
+++ js/user.js
@@ -8,7 +8,8 @@ let userInfo = {};
 
 const clientId = "_hmVVnvtB0bPAc6HaaZz";  // 네이버에서 발급받은 클라이언트 ID
 const redirectURI = `https://kinjokladgibkhadlkbnhkobkgokdfee.chromiumapp.org/provider_cb`;  // 네이버에서 설정한 콜백 URL
-const supabaseFunctionURL = 'https://ykorbmtrpjhatgnhbjbp.supabase.co/functions/v1/get-naver-token';
+const supabaseFunctionURL = 'https://ykorbmtrpjhatgnhbjbp.supabase.co/functions/v1/get-naver-token';    // 로그인 시도 supabase Function 요청 URL 
+const supabaseUserInfoURL = 'https://ykorbmtrpjhatgnhbjbp.supabase.co/functions/v1/get-user-info';      // 유저정보 가저오는 supabase Function 요청 URL
 
 document.querySelector(".loginButton").addEventListener("click", function () {
     const state = Math.random().toString(36).substr(2, 10); // 랜덤 상태 값 생성
@@ -24,13 +25,13 @@ document.querySelector(".loginButton").addEventListener("click", function () {
             const urlParams = new URLSearchParams(new URL(redirect_url).search);
             const code = urlParams.get('code');
             getAccessToken(code).then(accessToken => {
-                return getUserInfo(accessToken);
+                return fetchUserInfoFromSupabase(accessToken);
             }).catch(error => console.error(error));
         }
     });
 });
 
-// 콜백 URL에서 코드 받고 Supabase 함수로 토큰을 요청하는 코드
+// 콜백 URL에서 코드 받고 Supabase 로그인 시도 함수로 토큰을 요청하는 코드
 async function getAccessToken(code) {
     const response = await fetch(`${supabaseFunctionURL}?code=${code}`, {
         method: 'GET',
@@ -48,29 +49,28 @@ async function getAccessToken(code) {
     }
 }
 
-// 사용자 정보 요청
-async function getUserInfo(accessToken) {
-    const profileURL = "https://openapi.naver.com/v1/nid/me";
-    const response = await fetch(profileURL, {
+// Supabase에서 사용자 정보를 가져오는 코드
+async function fetchUserInfoFromSupabase(accessToken) {
+    const response = await fetch(`${supabaseUserInfoURL}?accessToken=${accessToken}`, {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrb3JibXRycGpoYXRnbmhiamJwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTg0ODU1MywiZXhwIjoyMDQxNDI0NTUzfQ.vzkgJHBJr4E2vTjAYjq08nbPK1Y9w0J4dLckOxJ9Udk'  
         },
     });
 
     const profileData = await response.json();
-    if (profileData.response) {
+    if (profileData.email) {
         userInfo = {
-            email: profileData.response.email,
-            nickname: profileData.response.nickname,
-            age: profileData.response.age,
+            email: profileData.email,
+            nickname: profileData.nickname,
+            age: profileData.age,
         };
-
-        document.getElementById("userNameHead").innerText = userInfo.nickname;
-        document.getElementById("userNameTail").innerText = userInfo.email; // Email 또는 다른 정보 표시
         inLogin = true;
 
         console.log(inLogin);
         console.log(userInfo);
+    } else {
+        console.error("Failed to fetch user info from Supabase");
     }
 }
---
+++ supa/supabase/functions/get-user-info/index.ts
@@ -0,0 +1,55 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve(async (req) => {
+    if (req.method === "OPTIONS") {
+      const headers = new Headers();
+      headers.set("Access-Control-Allow-Origin", "*");
+      headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      return new Response(null, { headers });
+    }
+    
+    // Authorization 헤더 확인
+    const authHeader = req.headers.get("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    
+    const url = new URL(req.url);
+    const accessToken = url.searchParams.get("accessToken");
+  
+    if (!accessToken) {
+      return new Response("Code not found", { status: 400 });
+    }
+  
+    try{
+      // Naver API 요청을 프록시
+      const profileURL = "https://openapi.naver.com/v1/nid/me";
+      const profileResponse = await fetch(profileURL, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      });
+
+      const profileData = await profileResponse.json();
+
+      if (!profileData.response) {
+        return new Response("Failed to fetch user info", { status: 400 });
+      }
+
+      const headers = new Headers();
+      headers.set("Content-Type", "application/json");
+      headers.set("Access-Control-Allow-Origin", "*");
+      headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+      return new Response(JSON.stringify(profileData.response), {
+        status: 200,
+        headers,
+      });
+    } catch (error) {
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  });
+  
