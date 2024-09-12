09e133783a3b43a88dd78ca55eeca84b23afbd38
강진수 <wide4514@ncubeworld.com>
2024-09-12 18:27:46 +0900

    로그인시 텍스트 변경하는 시퀀스 진행중

--- js/handler.js
+++ js/handler.js
@@ -1,9 +1,45 @@
+/**
+ * ---- user.js 에서 이어지는 변수 ----
+ * isLogin : 로그인 되었는지 아닌지 boolean
+ * userInfo : 유저 정보 객체 {
+ *  age : 나이 <- 필요없을듯?
+ *  email : 이메일 주소
+ *  nickname : 네이버 닉네임
+ * }
+ * ----
+ */
 
 /* DOM */
-
+const loginButton = document.querySelector(".loginButton");
+const userNameHead = document.querySelector(".userNameHead");
+const userNameTail = document.querySelector(".userNameTail");
 
 /* Function */
 
+/**
+ * @function changeLoginStateTextHandler :  로그인 상태에 따라 텍스트 변경하는 함수 
+ * @case_login 로그인으로 변경된 경우
+ * @case_logout 로그아웃으로 변경된 경우
+ */
+const changeLoginStateTextHandler = (status) => {
+    switch(status){
+        case "login" :
+            loginButton.innerText = "로그아웃"
+            userNameHead.innerText = userInfo.nickname
+            userNameTail.innerText = "님"
+            return
+        case "logout" :
+            loginButton.innerText = "로그인"
+            userNameHead.innerText = ""
+            userNameTail.innerText = "비로그인 상태"
+            return
+        default : 
+            loginButton.innerText = "로그아웃"
+            userNameHead.innerText = userInfo.nickname
+            userNameTail.innerText = "님"
+            return
+    }
+}
 
 
 /* Event-Handler */
--- js/user.js
+++ js/user.js
@@ -1,36 +1,46 @@
+
 /* local 정보로 남겨야할 요소
     1. 로그인 상태
     2. 유저 정보
 */
 
-let inLogin = false;
+let isLogin = false;
 let userInfo = {};
 
 const clientId = "_hmVVnvtB0bPAc6HaaZz";  // 네이버에서 발급받은 클라이언트 ID
-const redirectURI = `https://kinjokladgibkhadlkbnhkobkgokdfee.chromiumapp.org/provider_cb`;  // 네이버에서 설정한 콜백 URL
+const redirectURI = `https://jeconklphjefminemfpibahohjiookne.chromiumapp.org/provider_cb`;  // 네이버에서 설정한 콜백 URL
 const supabaseFunctionURL = 'https://ykorbmtrpjhatgnhbjbp.supabase.co/functions/v1/get-naver-token';    // 로그인 시도 supabase Function 요청 URL 
 const supabaseUserInfoURL = 'https://ykorbmtrpjhatgnhbjbp.supabase.co/functions/v1/get-user-info';      // 유저정보 가저오는 supabase Function 요청 URL
 
-document.querySelector(".loginButton").addEventListener("click", function () {
-    const state = Math.random().toString(36).substr(2, 10); // 랜덤 상태 값 생성
-    const apiURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectURI}&state=${state}`;
-
-    chrome.identity.launchWebAuthFlow({
-        url: apiURL,
-        interactive: true
-    }, function(redirect_url) {
-        if (chrome.runtime.lastError || redirect_url.includes('error')) {
-            console.error(chrome.runtime.lastError);
-        } else {
-            const urlParams = new URLSearchParams(new URL(redirect_url).search);
-            const code = urlParams.get('code');
-            getAccessToken(code).then(accessToken => {
-                return fetchUserInfoFromSupabase(accessToken);
-            }).catch(error => console.error(error));
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".loginButton").addEventListener("click", function () {
+        if(isLogin)
+        {
+            /* 이미 로그인 상태면 로그아웃 */
+            isLogin = false;
+            changeLoginStateTextHandler("logout")
+            return;
         }
+        
+        const state = Math.random().toString(36).substr(2, 10); // 랜덤 상태 값 생성
+        const apiURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectURI}&state=${state}`;
+
+        chrome.identity.launchWebAuthFlow({
+            url: apiURL,
+            interactive: true
+        }, function(redirect_url) {
+            if (chrome.runtime.lastError || redirect_url.includes('error')) {
+                console.error(chrome.runtime.lastError);
+            } else {
+                const urlParams = new URLSearchParams(new URL(redirect_url).search);
+                const code = urlParams.get('code');
+                getAccessToken(code).then(accessToken => {
+                    return fetchUserInfoFromSupabase(accessToken);
+                }).catch(error => console.error(error));
+            }
+        });
     });
 });
-
 // 콜백 URL에서 코드 받고 Supabase 로그인 시도 함수로 토큰을 요청하는 코드
 async function getAccessToken(code) {
     const response = await fetch(`${supabaseFunctionURL}?code=${code}`, {
@@ -66,9 +76,9 @@ async function fetchUserInfoFromSupabase(accessToken) {
             nickname: profileData.nickname,
             age: profileData.age,
         };
-        inLogin = true;
-
-        console.log(inLogin);
+        isLogin = true;
+            changeLoginStateTextHandler("login")
+        console.log(isLogin);
         console.log(userInfo);
     } else {
         console.error("Failed to fetch user info from Supabase");
